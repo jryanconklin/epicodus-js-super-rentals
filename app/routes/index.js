ab497fc38bfbaf603345736306a44e3eeb8edc21
J. Ryan Conklin and Stephen Emery <stephen@>
2016-10-24 10:51:49 -0700

    Add index.html and rental model.

---
+++ app/routes/index.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+//Dummy Data Exists Here, In Our Hearts
+
+export default Ember.Route.extend({
+
+//Include Commas on the exported route, it is an object.
+  model() {
+    return rentals;
+  },
+
+
+});
