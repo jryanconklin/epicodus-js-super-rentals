89dfca2409b9198a62d10a29150331cadf4fcdcc
J. Ryan Conklin and Stephen Emery <stephen@>
2016-10-24 13:12:43 -0700

    Add delete functionality.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,16 @@ import Ember from 'ember';
 //Dummy Data Exists Here, In Our Hearts
 
 export default Ember.Route.extend({
-
 //Include Commas on the exported route, it is an object.
   model() {
     return this.store.findAll('rental');
   },
+
+  actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+
 });
